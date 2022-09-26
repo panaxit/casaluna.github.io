6156f63ff812a5c2d4ce8e6672cf905e7bdf4670
Uriel Gomez <uriel@panax.io>
2022-09-25 23:24:32 -0500

    Renombramiento de variables en listener

--- custom.js
+++ custom.js
@@ -1,22 +1,22 @@
-xo.listener.on(`change::px:Entity/data:rows/xo:r/@*`, function ({ element, attribute, old, value }) {
+xo.listener.on(`change::px:Entity/data:rows/xo:r/@*`, function ({ element:row, attribute, old, value }) {
     let entity = attribute.$('ancestor::px:Entity[1]')
     switch (entity.get("Schema") + '.' + entity.get("Name")) {
         case "Egresos.Gastos":
             if (['Total', 'TotalPagado'].includes(attribute.name)) {
-                element.set("Saldo", element.get("Total") - element.get("TotalPagado"))
+                row.set("Saldo", row.get("Total") - row.get("TotalPagado"))
             }
             break;
         case "Egresos.Compras":
-            let Cantidad = element.get("Cantidad");
-            let TotalCompraPesos = element.get("TotalCompraPesos");
-            let TotalCompraDolares = element.get("TotalCompraDolares");
-            element.set("PrecioUnitarioPesos", TotalCompraPesos / Cantidad)
-            element.set("PrecioUnitarioDolares", TotalCompraDolares / Cantidad)
+            let Cantidad = row.get("Cantidad");
+            let TotalCompraPesos = row.get("TotalCompraPesos");
+            let TotalCompraDolares = row.get("TotalCompraDolares");
+            row.set("PrecioUnitarioPesos", TotalCompraPesos / Cantidad)
+            row.set("PrecioUnitarioDolares", TotalCompraDolares / Cantidad)
             break;
         case "Ingresos.Venta":
-            let TotalArticulos = element.get("Monto");
-            let Descuento = element.get("Descuento");
-            element.set("MontoTotal", TotalArticulos - Descuento)
+            let TotalArticulos = row.get("Monto");
+            let Descuento = row.get("Descuento");
+            row.set("MontoTotal", TotalArticulos - Descuento)
             break;
         default:
             break;
